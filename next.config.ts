9ef1200b3a024428d3faeae91fe664dc1a570eb6
viktoria yevpak <viktoriia.yevpak@cosmonova-broadcast.tv>
2025-02-26 15:21:27 +0200

    final changes

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,11 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-
     basePath: isProd ? "/frontend-next.js-mui-project" : "",
     assetPrefix: isProd ? "/frontend-next.js-mui-project/" : "",
+    images: {
+        unoptimized: true, // Вимикає автоматичну оптимізацію Next.js
+    },
 };
 
 export default nextConfig;
--- src/app/allPagesComponents/components/ImageBackground.tsx
+++ src/app/allPagesComponents/components/ImageBackground.tsx
@@ -1,17 +1,17 @@
-import {Box} from "@mui/material";
+import { Box } from "@mui/material";
 
-export default function ImageBackground(){
+export default function ImageBackground() {
 
-    const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
-
-    return <Box
-        component="img"
-        src={`${basePath}/image.svg`}
-        alt="Background Image"
-        sx={{
-            width: 'auto%',
-            height: "100vh",
-            filter: "brightness(50%)",
-        }}
-    ></Box>
+    return (
+        <Box
+            component="img"
+            src='/image.svg'
+            alt="Background Image"
+            sx={{
+                width: "100%",
+                height: "100vh",
+                filter: "brightness(50%)",
+            }}
+        />
+    );
 }
